bf1f4f7d29c6763463967ead2d01c41670ed3fdc
Brandon Suarez <brandonsuarezduran@gmail.com>
2023-11-17 12:34:12 -0500

    Initial Commit

---
+++ NetworkMonitorTestUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkMonitorTestUIKit
+//
+//  Created by Brandon Suarez on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
